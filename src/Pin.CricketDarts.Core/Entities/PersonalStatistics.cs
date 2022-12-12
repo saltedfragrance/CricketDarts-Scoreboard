0e6ddc9a198b7171eab9e41392b79d740473f70d
stijn <stijn.vandekerckhove2@student.howest.be>
2022-12-12 10:41:29 +0100

    Add PersonalStatistics entity

---
+++ src/Pin.CricketDarts.Core/Entities/PersonalStatistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Entities
+{
+    public class PersonalStatistics
+    {
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+        public int TriplesThrown { get; set; }
+        public int DoublesThrown { get; set; }
+    }
+}
