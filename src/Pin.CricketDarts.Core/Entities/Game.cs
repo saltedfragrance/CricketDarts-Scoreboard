4c4cab9e14088de22bd3eeb161fc2a386e42a8b8
stijn <stijn.vandekerckhove2@student.howest.be>
2022-12-12 09:50:39 +0100

    Add Game entity

---
+++ src/Pin.CricketDarts.Core/Entities/Game.cs
@@ -0,0 +1,14 @@
+﻿using Pin.CricketDarts.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Entities
+{
+    public class Game : BaseEntity
+    {
+        public ICollection<Player> Players { get; set; }
+    }
+}
---
+++ src/Pin.CricketDarts.Core/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿using Pin.CricketDarts.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Entities
+{
+    public class Player : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
