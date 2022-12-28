2e00136bc310ec2ce0ce015eee9d8a708ec8b8f6
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-28 22:40:11 +0100

    Add CreatePlayer to PlayerService

---
+++ src/Pin.CricketDarts.Server/Services/Api/PlayerService.cs
@@ -0,0 +1,36 @@
+﻿using Pin.CricketDarts.Server.Models;
+using Pin.CricketDarts.Server.Services.Interfaces;
+using Pin.CricketDarts.Shared;
+
+namespace Pin.CricketDarts.Server.Services.Api
+{
+    public class PlayerService : IPlayerService
+    {
+        private string baseUrl = "https://localhost:7117/api/Players";
+        private readonly HttpClient _httpClient;
+
+        public PlayerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task CreatePlayer(Player player)
+        {
+            var dto = new PlayerRequestDto
+            {
+                Name = player.Name,
+                Id = Guid.NewGuid(),
+                PersonalStatistics = new PersonalStatisticsRequestDto
+                {
+                    Id = Guid.NewGuid(),
+                    DoublesThrown = 0,
+                    GamesLost = 0,
+                    GamesWon = 0,
+                    TriplesThrown = 0,
+                }
+            };
+
+            return _httpClient.PostAsJsonAsync<PlayerRequestDto>(baseUrl, dto);
+        }
+    }
+}
