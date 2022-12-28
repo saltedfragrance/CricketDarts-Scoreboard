6e8e536059bfe3ce4224184f701530a421528114
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-28 20:12:32 +0100

    Add IRepository base interface

---
+++ src/Pin.CricketDarts.Core/Interfaces/Base/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pin.CricketDarts.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Base
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(Guid id);
+    }
+}
--- src/Pin.CricketDarts.Core/Interfaces/IPlayerRepository.cs
+++ src/Pin.CricketDarts.Core/Interfaces/IPlayerRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Pin.CricketDarts.Core.Interfaces
 {
-    internal class IPlayerRepository
+    public interface IPlayerRepository : IRepository<Player>
     {
+        Task<IEnumerable<Player>> GetPlayers();
     }
 }
