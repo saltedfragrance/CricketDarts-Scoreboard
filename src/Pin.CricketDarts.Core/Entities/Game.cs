049c5fd8ed1579dddd7d179967037776692a79d1
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-29 20:09:11 +0100

    Add Tournament entity

--- src/Pin.CricketDarts.Core/Entities/Game.cs
+++ src/Pin.CricketDarts.Core/Entities/Game.cs
@@ -9,6 +9,8 @@ namespace Pin.CricketDarts.Core.Entities
 {
     public class Game : BaseEntity
     {
+        public Guid TournamentId { get; set; }
+        public Tournament Tournament { get; set; }
         public bool IsActive { get; set; } = false;
         public ICollection<PlayerGames> PlayerGames { get; set; }
         public Guid WinnerId { get; set; }
