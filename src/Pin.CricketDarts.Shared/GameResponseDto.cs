5dd71278229c4312183b8c1a7ddec693c0a31c46
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-29 17:30:20 +0100

    Add GameResponseDto

---
+++ src/Pin.CricketDarts.Shared/GameResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Pin.CricketDarts.Shared
+{
+    public class GameResponseDto
+    {
+        public Guid TournamentId { get; set; }
+        public bool IsActive { get; set; } = false;
+        public IEnumerable<PlayerResponseDto> Players { get; set; }
+        public Guid WinnerId { get; set; }
+        public IEnumerable<ScoreResponseDto> Scores { get; set; }
+    }
+}
