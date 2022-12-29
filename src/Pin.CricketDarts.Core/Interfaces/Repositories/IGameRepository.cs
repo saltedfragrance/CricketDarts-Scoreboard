90dbe9279f692ca8095aa5d162e2702246a97be3
Stijn Vandekerckhove <stijn.vandekerckhove2@student.howest.be>
2022-12-29 17:28:30 +0100

    Add IGameService

--- src/Pin.CricketDarts.Core/Interfaces/Repositories/IGameRepository.cs
+++ src/Pin.CricketDarts.Core/Interfaces/Repositories/IGameRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pin.CricketDarts.Core.Entities;
+using Pin.CricketDarts.Core.Interfaces.Repositories.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Pin.CricketDarts.Core.Interfaces.Repositories
 {
-    internal class IGameRepository
+    public interface IGameRepository : IRepository<Game>
     {
     }
 }
---
+++ src/Pin.CricketDarts.Core/Interfaces/Services/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using Pin.CricketDarts.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.CricketDarts.Core.Interfaces.Services
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<GameResponseDto>> GetAllAsync();
+        Task<GameResponseDto> GetByIdAsync(Guid id);
+        Task AddAsync(GameRequestDto gameRequestDto);
+        Task<GameResponseDto> UpdateAsync(GameRequestDto gameRequestDto);
+    }
+}
